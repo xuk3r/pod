55e129549af6e3a9ba1673e5a5f8df2af14bebd5
xuk3r <a4101823@qq.com>
2019-03-12 14:45:28 +0800

    no message

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by 徐克 on 2019/3/12.
+//  Copyright © 2019 5th. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ pod/pod/PodClass.swift
@@ -0,0 +1,15 @@
+//
+//  PodClass.swift
+//  pod
+//
+//  Created by 徐克 on 2019/3/12.
+//  Copyright © 2019 5th. All rights reserved.
+//
+
+import UIKit
+
+class PodClass: NSObject {
+    open class func log(){
+        print("PodClass")
+    }
+}
